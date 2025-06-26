8d2e1b756468a1e4b8aa9c1a6da853e47a754b43
rajavardhan <ravularajavardhan96@gmail.com>
2025-06-26 12:50:15 +0530

    frontend also working

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -23,7 +23,7 @@ const Dashboard = () => {
           <Route path="/orders" element={<Orders />} />
           <Route path="/holdings" element={<Holdings />} />
           <Route path="/positions" element={<Positions />} />
-          <Route path="/funds" element={<Funds />} />
+          <Route path="funds" element={<Funds />} />
           <Route path="/apps" element={<Apps />} />
         </Routes>
       </div>
